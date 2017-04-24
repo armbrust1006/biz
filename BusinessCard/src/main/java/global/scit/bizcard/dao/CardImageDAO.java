37901a1d9e10b984a4555c79677bb451601b87f8
Unknown <creativetelly@gmail.com>
2017-04-24 17:51:56 +0900

    근환상+현택상 합친 것(4월 24일) 자신의 것을 포맷하고 pull하세요

--- BusinessCard/src/main/java/global/scit/bizcard/dao/CardImageDAO.java
+++ BusinessCard/src/main/java/global/scit/bizcard/dao/CardImageDAO.java
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-package global.scit.bizcard.dao;
-import global.scit.bizcard.vo.Card;
-import global.scit.bizcard.vo.CardImage;
-
-public interface CardImageDAO {
-
-	public CardImage myCardExist(String m_id) throws Exception;
-
-	public int saveCardImage(CardImage cardImage) throws Exception;
-
-	public int setMyCardList(CardImage cardImage) throws Exception;
-
-	public int sharedChange(CardImage cardImage) throws Exception;
-
-	public int getImageNumber(String m_id) throws Exception;
-
-	public int loadCardImage(int cardNum) throws Exception;
-
-	public String checkExistMine(String id) throws Exception;
-
-	public Card setMyCardSharing(CardImage cardImage) throws Exception;
-}
-=======
 package global.scit.bizcard.dao;
 
 import global.scit.bizcard.vo.Card;
@@ -47,4 +23,3 @@ public interface CardImageDAO {
 
 	public CardImage myCardExist(String m_id) throws Exception;
 }
->>>>>>> master
