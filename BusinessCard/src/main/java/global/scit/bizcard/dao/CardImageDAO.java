2200f47bfcb45546db341161a4ebdd7b995638e4
Inkyeong Yang <keiink07@gmail.com>
2017-04-24 09:12:28 +0900

    인경
    2017-04-24

--- BusinessCard/src/main/java/global/scit/bizcard/dao/CardImageDAO.java
+++ BusinessCard/src/main/java/global/scit/bizcard/dao/CardImageDAO.java
@@ -19,23 +19,3 @@ public interface CardImageDAO {
 
 	public Card setMyCardSharing(CardImage cardImage) throws Exception;
 }
-
-import global.scit.bizcard.vo.Card;
-import global.scit.bizcard.vo.CardImage;
-
-public interface CardImageDAO {
-
-	public int saveCardImage(CardImage cardImage) throws Exception;
-
-	public int setMyCardList(CardImage cardImage) throws Exception;
-
-	public int sharedChange(CardImage cardImage) throws Exception;
-
-	public int getImageNumber(String m_id) throws Exception;
-
-	public int loadCardImage(int cardNum) throws Exception;
-
-	public String checkExistMine(String id) throws Exception;
-
-	public Card setMyCardSharing(CardImage cardImage) throws Exception;
-}
